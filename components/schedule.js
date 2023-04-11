c3c1eedead11f3fa43970a762c3e8f8484ea79b1
christian595 <christiangirgenti595@gmail.com>
2023-04-11 02:49:04 +0100

    Trying to make schedule draft work, WIP

--- components/schedule.js
+++ components/schedule.js
@@ -3,6 +3,7 @@ import {
   View, Text, TextInput, StyleSheet, TouchableOpacity,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import GlobalStyle from '../styles/GlobalStyle';
 import NavigationHeaderWithIcon from './screenForNavigation/navigationHeaderWithIcon';
 
@@ -36,14 +37,60 @@ export default class Schedule extends Component {
     this.setState({ error: '' });
   }
 
-  schedule() {
+  async send(chatId, draft) {
+    const now = new Date();
+    // Create a new Date object with no arguments to represent the current time
+    const hours = now.getHours(); // Get the current hour (0-23)
+    const minutes = now.getMinutes(); // Get the current minute (0-59)
+    const seconds = now.getSeconds(); // Get the current second (0-59)
+    console.log(`The current time is ${hours}:${minutes}:${seconds}`); // Output: "The current time is 14:30:45" (for example)
+    this.clearErrorMessages();
+    const toSend = {
+      message: draft,
+    };
+    return fetch(
+      `http://localhost:3333/api/1.0.0/chat/${chatId}/message`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Authorization': await AsyncStorage.getItem('whatsthat_session_token'),
+        },
+        body: JSON.stringify(toSend),
+      },
+    )
+      .then(async (response) => {
+        if (response.status === 200) {
+          console.log('Draft Sent');
+        } else if (response.status === 401) {
+          console.log('Unauthorised');
+          await AsyncStorage.removeItem('whatsthat_session_token');
+          await AsyncStorage.removeItem('whatsthat_user_id');
+          this.navigation.navigate('Login');
+        } else if (response.status === 400) throw new Error('You can not create the new chat');
+        else throw new Error('Something went wrong while creating the new chat');
+      })
+      .catch((thisError) => {
+        this.setState({ error: thisError.message });
+      });
+  }
+
+  async schedule() {
+    const now = new Date();
+    // Create a new Date object with no arguments to represent the current time
+    const hours = now.getHours(); // Get the current hour (0-23)
+    const minutes = now.getMinutes(); // Get the current minute (0-59)
+    const seconds = now.getSeconds(); // Get the current second (0-59)
+    console.log(`The current time is ${hours}:${minutes}:${seconds}`); // Output: "The current time is 14:30:45" (for example)
+
     this.clearErrorMessages();
     if (!this.validateDateAndTime()) {
       this.setState({ error: 'Date or Time format are not valid' });
       return;
     }
-    const thisDate = new Date(`${this.state.time} ${this.state.date}`);
-    console.log(thisDate);
+    const sendTime = new Date(`${this.state.time} ${this.state.date}`);
+    const currentTime = new Date().getTime();
+    const delay = sendTime - currentTime;
   }
 
   render() {
@@ -73,7 +120,7 @@ export default class Schedule extends Component {
           </View>
           <View style={styles.form}>
             <TouchableOpacity style={GlobalStyle.button} onPress={() => this.schedule()}>
-              Send
+              <Text style={GlobalStyle.buttonText}>Send</Text>
             </TouchableOpacity>
           </View>
         </View>
